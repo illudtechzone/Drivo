7d07d4ebe1a992c4fe71e821ed2ce2abf9f6dc3c
sooraj_pn <you@example.com>
2019-08-22 16:46:24 +0530

    workflow completed for driver

--- src/app/pages/invoice/invoice.page.ts
+++ src/app/pages/invoice/invoice.page.ts
@@ -1,3 +1,6 @@
+import { NavController } from '@ionic/angular';
+import { CommandResourceService } from 'src/app/api/services/command-resource.service';
+import { QueryResourceService } from 'src/app/api/services';
 import { CustomerService } from './../../services/customerService/customer.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,16 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoicePage implements OnInit {
 
-  travelInfo:any={}
-
-  constructor(private customerService:CustomerService) {
+  travelInfo: any = {};
+  processInstanceId= '';
+  constructor(private customerService: CustomerService,
+              private queryResourceService: QueryResourceService,
+              private commandResourceService: CommandResourceService,
+              private navCtrl: NavController) {
 
    }
 
   ngOnInit() {
 
-    this.travelInfo=this.customerService.getRiderLocationInfo();
+    this.travelInfo = this.customerService.getRiderLocationInfo();
+    this.processInstanceId = this.travelInfo.trackingProcessinstanceId;
+    console.log('process instace id ', this.processInstanceId);
 
   }
 
+  cashCollected() {
+    this.getTasks();
+  }
+  getTasks() {
+    console.log('process instace id ', this.processInstanceId);
+    this.queryResourceService.getTasksUsingGET({processInstanceId: this.processInstanceId}).subscribe(
+      (res: any) => {
+        console.log('got tasks ', res);
+        this.commandResourceService.paymentUsingPOST({taskId: res.data[0].id, paymentStatus: {paymentStatus: 'paid'}}).subscribe(
+          res2 => {
+            console.log('payment completed ', res2);
+            this.navCtrl.navigateForward('/home');
+          }
+        );
+      }
+      );
+
+  }
 }
